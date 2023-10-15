61605fc234dcfad76b69cc301bae2f37d4c640eb
Trendswave <paulchinedumicheal@gmail.com>
2023-10-15 10:11:04 -0700

    unix header files

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <signal.h>
