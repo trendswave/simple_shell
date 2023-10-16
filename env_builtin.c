79962796d21747ec2310c464a33a5dee24f70490
oladakinjoye <oladakinjoye@gmail.com>
2023-10-16 09:12:12 -0700

    env_builtin.c file update 1

---
+++ env_builtin.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * env - to print the environment variables in the format "variable=value"
+ * Description: function iterates through the environ array
+ *
+ * Return: void
+ */
+
+void env(void)
+{
+	int j = 0;
+
+	while (environ[j])
+	{
+		/* print in form of "variable=value" */
+		write(STDOUT_FILENO, environ[j], _strlen(environ[j]));
+		j++;
+		write(STDOUT_FILENO, "\n", 1);
+	}
+}
