6a5c122db8a60b8af057faaa89612fafb5fa1635
Trendswave <paulchinedumicheal@gmail.com>
2023-10-17 00:38:28 -0700

    the function that handle error messge

---
+++ error_msg.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+/**
+*error - This function returns custom error messages
+* @str: The argument at index 0
+* @line: Line count from getline function
+* @flag: To determine which error message to output
+*/
+
+void error(char *str, int ln, int flag)
